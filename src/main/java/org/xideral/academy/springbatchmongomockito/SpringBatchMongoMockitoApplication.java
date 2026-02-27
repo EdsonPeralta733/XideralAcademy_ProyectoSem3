2e92f6efc223de9e148a76616f034c78fa111ba8
EdsonPeralta733 <edsonmiguel.peraltavaldez733@gmail.com>
2026-02-27 17:59:45 -0600

    Creacion del Proyecto de la semana 3 - SpringBatchMongoMockito

---
+++ src/main/java/org/xideral/academy/springbatchmongomockito/SpringBatchMongoMockitoApplication.java
@@ -0,0 +1,13 @@
+package org.xideral.academy.springbatchmongomockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchMongoMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchMongoMockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/xideral/academy/springbatchmongomockito/SpringBatchMongoMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xideral.academy.springbatchmongomockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchMongoMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
